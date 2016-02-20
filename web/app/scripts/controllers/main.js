8577403cb5d2a7890b5ce838b48c438468ed2b90
jherrlin <jherrlin@gmail.com>
2016-02-20 14:31:04 +0100

    yoeman angular project started

---
+++ web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pimraApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pimraApp
+ */
+angular.module('pimraApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
