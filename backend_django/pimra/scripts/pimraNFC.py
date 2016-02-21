7dc3fae656d97a0e5d9cf67c8cfadd5a598141df
John Herrlin <jherrlin@gmail.com>
2016-02-21 11:55:30 +0100

    test of dump

--- backend_django/pimra/scripts/pimraNFC.py
+++ backend_django/pimra/scripts/pimraNFC.py
@@ -2,7 +2,7 @@
 import subprocess
 from datetime import datetime
 
-DUMP_PATH = "/home/root/nfcstuff/Pimra/backend_django/dumps/"
+DUMP_PATH = "/root/nfcstuff/Pimra/backend_django/dumps/"
 DEFAULT_NAME = datetime.now().strftime("%y-%m-%d-%H:%M")
 
 
@@ -25,7 +25,7 @@ def nfc_dump(dump_name=DEFAULT_NAME):
     print('Running nfc-mfclassic')
     save_path = DUMP_PATH + "full/"
     print('Saving dump to: '+save_path + dump_name)
-    cmd = "nfc-mfclassic r a /home/root/nfcstuff/Pimra/backend_django/dumps/full/test1.mfd"
+    cmd = "nfc-mfclassic r a /root/nfcstuff/Pimra/backend_django/dumps/full/test1.mfd"
 
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
 
